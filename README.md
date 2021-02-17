a0c1b4d1362df03058961ee6f0fb6bd57c84ce19
kil <godngu@gmail.com>
2021-02-17 11:29:59 +0900

    입출금내역 분석기 파싱로직 분리

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Real-World-Software-Development
+# 책 실습용 프로젝트 입니다.
+## "실전 자바 소프트웨어 개발 : 4가지 프로젝트로 배우는 최신 자바 개발 기법"
+
+![](./img/book_real-world-software-development.jpg)
+
--- src/main/java/com/example/book/chapter02/BankTransactionAnalyzerSimple.java
+++ src/main/java/com/example/book/chapter02/BankTransactionAnalyzerSimple.java
@@ -4,23 +4,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Month;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
 
     private static final String RESOURCES = "src/main/resources";
 
+    private static final BankStatementCSVParser parser = new BankStatementCSVParser();
+
     public static void main(String[] args) throws IOException {
         final String fileName = "/bank-data-simple.csv";
         final Path path = Paths.get(RESOURCES + fileName);
         final List<String> lines = Files.readAllLines(path);
-        double total = 0d;
-        for (String line : lines) {
-            final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
-        }
 
-        System.out.println(String.format("The total for all transactions is %f", total));
+        List<BankTransaction> bankTransactions = parser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+
+    private static Double calculateTotalAmount(List<BankTransaction> bankTransactions) {
+        return bankTransactions.stream()
+            .mapToDouble(BankTransaction::getAmount)
+            .sum();
+    }
+
+    private static Double selectInMonth(List<BankTransaction> bankTransactions, Month january) {
+        double total = 0d;
+        for (BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == january) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
     }
 }
